966e7296807a7ee4900086b5b89eb4e614be57e8
Adeosun Oluwaseyi <deosundeola@gmail.com>
2023-08-08 10:42:57 -0700

    Airbnb

---
+++ models/base_model.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python3
+"""
+A class BaseModel that defines all common attributes/methods
+for other classes.
+"""
+
+import uuid
+from datetime import datetime
+
+
+class BaseModel:
+    """
+    A class BaseModel
+
+    Attributes:
+        id: public class attribute that generate a unique id
+        created_at: public class instance that assigns the current
+                    datetime when an instance is created.
+        updated_at: public class instance that assigns the current
+                    datetime when an instance is created and updated
+                    everytime it changes at object.
+    """
+    def __init__(self, args, **kwargs):
+        """
+        Initialising public instance attributes.
+
+        Args:
+            args: numbers od argument.
+            kwargs: A double pointer to the head or an array of args.
+        """
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+
+    def save(self):
+        """
+        updates the public instance attribute updated_at.
+        """
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        """
+        Returns:
+            A dictionary containing all keys/values of __dict__.
+        """
+        d = self.__dict__.copy()
+        d['__class__'] = self.__class__.__name__
+        d['created_at'] = self.created_at.isoformat()
+        d['updated_at'] = self.updated_at.isoformat()
+        return d
+
+    def __str__(self):
+        """
+        print:
+            [<class name>] (<self.id>) <self.__dict__>
+        """
+        return f"[BaseModel] ({self.id}) {self.__dict__}>"
