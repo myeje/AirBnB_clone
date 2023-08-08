217581e0b1de32f1151a7763678942dc884c0dd5
Joshua Emore <ehimareemore@gmail.com>
2023-08-08 10:57:54 -0700

    Airbnb

--- models/base_model.py
+++ models/base_model.py
@@ -20,7 +20,7 @@ class BaseModel:
                     datetime when an instance is created and updated
                     everytime it changes at object.
     """
-    def __init__(self, args, **kwargs):
+    def __init__(self, *args, **kwargs):
         """
         Initialising public instance attributes.
 
@@ -28,9 +28,19 @@ class BaseModel:
             args: numbers od argument.
             kwargs: A double pointer to the head or an array of args.
         """
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == "__class__":
+                    continue
+                if key == "created_at" or key == "updated_at":
+                    format_string = "%Y-%m-%dT%H:%M:%S.%f"
+                    setattr(self, key, datetime.strptime(value, format_string))
+                else:
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = self.created_at
 
     def save(self):
         """
