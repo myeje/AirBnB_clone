c3a44ac2fd9ae76c5ca485c1d97147b15f220a12
Adeosun Oluwaseyi <deosundeola@gmail.com>
2023-08-12 19:24:16 -0700

    changes

--- console.py
+++ console.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
-"""
-Class console command that opens cmd interface to edit and update files
-"""
+"""creating the command interpreter"""
+
+
 import cmd
 from models import storage
-from models.engine.file_storage import FileStorage
 from models.base_model import BaseModel
 from models.user import User
 from models.state import State
@@ -12,206 +11,172 @@ from models.city import City
 from models.amenity import Amenity
 from models.place import Place
 from models.review import Review
+import json
 
 
 class HBNBCommand(cmd.Cmd):
+    """
+    the console
+    """
     prompt = "(hbnb) "
-    classes = [
-        "BaseModel",
-        "User",
-        "State",
-        "City",
-        "Amenity",
-        "Place",
-        "Review"
-    ]
 
-    def do_quit(self, args):
-        """Quit command to exit the program
-        """
-        return True
-
-    def do_EOF(self, args):
-        """EOF command to exit the program (CTRL + D)"""
-        print("")
-        return True
+    __classes = [
+        'BaseModel', 'User', 'State', 'City', 'Amenity', 'Place', 'Review'
+        ]
 
     def emptyline(self):
-        """Override emptyline to do nothing"""
-        return False
+        pass
+
+    def do_quit(self, arg):
+        raise SystemExit
+
+    def do_EOF(self, arg):
+        raise SystemExit
 
     def help_quit(self):
-        """
-        Display help for the quit command.
-        """
-        print("Quit the command to exit the program\n")
+        print("Quit command used to exit the program")
+        print()
 
     def help_EOF(self):
-        """
-        Display help for the EOF command.
-        """
-        print("Exit the command interpreter using EOF (Ctrl+D)\n")
+        print("EOF command used to exit the program")
 
-    def help_help(self):
+    def do_create(self, arg):
         """
-        Display help for the help command
-        """
-        print("Show help for the available commands\n")
-
-    def do_create(self, args):
-        """
-        Create a new instance of User and saves it to a JSON file.
-        Usage: create User.
-        """
-        if not args:
+        Methods that is responsible for creating a new instance
+        based on the class and saves it in a json file and prints
+        the ID"""
+        if arg == "":
             print("** class name missing **")
-            return
-        arg = args.split()
-        if arg[0] not in self.classes:
+        elif arg not in self.__classes:
             print("** class doesn't exist **")
-            return
-        new = eval(arg[0])()
-        new.save()
-        print(new.id)
+        else:
+            new_create = eval(arg)()
+            new_create.save()
+            print(new_create.id)
 
-    def help_create(self):
-        """
-        Display the help for the create command.
-        """
-        print("Create a new instance of a specified"
-              "class and save it to a JSON file")
-
-    def do_show(self, args):
-        """
-        Display the string representation of an instance.
-        Usage: show User <id>
-        """
-        if not args:
-            print(f"** class name missing **")
-            return
-        arg = args.split()
-        if arg[0] not in self.classes:
+    def do_show(self, arg):
+        """Prints the string representation of an
+        instance based on the class name and id"""
+        args = arg.split(" ")
+        if arg == '':
+            print("** class name missing **")
+        elif args[0] not in self.__classes:
             print("** class doesn't exist **")
-            return
-        if len(arg) < 2:
-            print(f"** instance id missing **")
-            return
-        storage = FileStorage()
-        obj_dict = storage.all()
-        obj_key = f"{arg[0]}.{arg[1]}"
-        if obj_key not in obj_dict:
-            print("** no instance found **")
-            return
-        print(obj_dict[obj_key])
-        if act == "show":
-            print(obj_dict[obj_key])
-            storage.save()
+        elif len(args) < 2:
+            print("** instance id missing **")
+        else:
+            id_instance = storage.all()
+            string = "{}.{}".format(args[0], args[1])
+            if string not in id_instance:
+                print("** no instance found **")
+            else:
+                print("{}".format(id_instance[string]))
 
-    def help_show(self):
-        """
-        Display help for the show command.
-        """
-        print("Display the string representation of an instance")
+    def do_destroy(self, arg):
+        """removes an instance based on class
+           name, ID and saves it to a json file"""
+        args = arg.split(" ")
+        if arg == '':
+            print("** class name missing **")
+        elif args[0] not in self.__classes:
+            print("** class doesn't exist **")
+        elif len(args) < 2:
+            print("** instance id missing **")
+        else:
+            id_instance = storage.all()
+            string = "{}.{}".format(args[0], args[1])
+            if string not in id_instance:
+                print("** no instance found **")
+            else:
+                del (id_instance[string])
+                storage.save()
 
-    def do_destroy(self, args):
+    def do_all(self, arg):
         """
-        Deletes an instance based on the class name and id.
-        Usage: destroy User <id>
+        methods that prints all string representations
+        of all instances based or not on class name
         """
-        self._validate_destroy(args, "destroy")
-
-    def help_destroy(self):
-        """
-        Display help for the destroy command.
-        """
-        print("Delete an instance based on the class name and id")
-
-    def do_all(self, args):
-        """
-        Display string representation of all instance or all instance
-        of a specific class.
-        Usage: all[class_name]
-        """
-        storage = FileStorage()
-        obj_dict = storage.all()
-        if not args:
-            for obj_key, obj in obj_dict.items():
-                print(obj)
-        elif args in self.classes:
-            for obj_key, obj in obj_dict.items():
-                if obj_key.split('.')[0] == args:
-                    print(obj)
+        list = []
+        all_directory = storage.all()
+        if arg == "":
+            for keys, values in all_directory.items():
+                list.append(str(values))
+            print(list)
+        elif arg in self.__classes:
+            for keys, values in all_directory.items():
+                if values.__class__.__name__ == arg:
+                    list.append(str(values))
+            print(list)
         else:
             print("** class doesn't exist **")
 
-    def help_all(self):
+    def do_update(self, arg):
         """
-        Display help for all command
+        methods to update an instance based on class name and
+        id by adding or updating the attribute that saves the
+        change in the json file
         """
-        print("Display string representations of all instances,"
-              "or all instances of a specific class")
-        print("Usage: all[class_name]")
-
-    def do_update(self, args):
-        """
-        Update an instance based on the class name and id.
-        Usage: update User <id> <attribute name> "<attribute value>"
-        """
-        self._validate_update(args)
-
-    def help_update(self):
-        """
-        Display help for the update command.
-        """
-        print("Update an instance based on the class name and id")
-
-    def _validate_destroy(self, args, act):
-        if not args:
-            print(f"** class name missing **")
-            return
-        arg = args.split()
-        if arg[0] not in self.classes:
-            print("** class doesn't exist **")
-            return
-        if len(arg) < 2:
-            print(f"** instance id missing **")
-            return
-        storage = FileStorage()
-        obj_dict = storage.all()
-        obj_key = f"{arg[0]}.{arg[1]}"
-        if obj_key not in obj_dict:
-            print("** no instance found **")
-            return
-        if act == "destroy":
-            del obj_dict[obj_key]
-            storage.save()
-
-    def _validate_update(self, args):
-        if not args:
+        args = arg.split(" ")
+        if arg == '':
             print("** class name missing **")
-            return
-        arg = args.split()
-        if arg[0] not in self.classes:
-            print("** class doesn't  exist **")
-            return
-        if len(arg) < 2:
+        elif args[0] not in self.__classes:
+            print("** class doesn't exist **")
+        elif len(args) == 1:
             print("** instance id missing **")
-            return
-        storage = FileStorage()
-        obj_dict = storage.all()
-        obj_key = f"{arg[0]}.{arg[1]}"
-        if obj_key not in obj_dict:
-            print("** no instance found **")
-            return
-        if len(arg) < 3:
-            print("** attribute name missing **")
-            return
-        if len(arg) < 4:
-            print("** value missing **")
-            return
-        instance = obj_dict[obj_key]
-        setattr(instance, args[2], args[3])
-        instance.save()
+        else:
+            id_instance = storage.all()
+            string = "{}.{}".format(args[0], args[1])
+            if string not in id_instance:
+                print("** no instance found **")
+            elif len(args) == 2:
+                print("** attribute name missing **")
+            elif len(args) == 3:
+                print("** value missing **")
+            else:
+                unmodifiable = ["id", "created_at", "updated_at"]
+                if args[2] not in unmodifiable:
+                    for key, value in id_instance.items():
+                        setattr(value, args[2], eval(args[3]))
+                        value.save()
+
+    def do_count(self, arg):
+        """Instance counter according to class"""
+        args = arg.split(" ")
+        if arg not in self.__classes:
+            print("** class doesn't exist **")
+        else:
+            list_new = []
+            list_dictionary = storage.all()
+            for key, value in list_dictionary.items():
+                if args[0] in key:
+                    list_new.append(value)
+            print(len(list_new))
+
+    def default(self, arg):
+        """
+        cmd method to validate when it does not
+        recognize the prefix of the command.
+        """
+        args = arg.split(".")
+        separator = args[1].split("(")
+        new_separator = separator[1].split(")")
+        new = new_separator[0].split(",")
+        if args[0] in self.__classes:
+            if args[1] == "all()":
+                nameClass = args[0]
+                return self.do_all(nameClass)
+            elif args[1] == "count()":
+                nameClass = args[0]
+                return self.do_count(nameClass)
+            elif args[1][0:4] == 'show':
+                arguments = args[0] + " " + new_separator[0]
+                return self.do_show(arguments)
+            elif args[1][0:7] == 'destroy':
+                arguments = args[0] + " " + new_separator[0]
+                return self.do_destroy(arguments)
+            elif args[1][0:6] == "update":
+                arguments = args[0] + " " + new[0] + "" + new[1] + "" + new[2]
+                return self.do_update(arguments)
 
 
 if __name__ == '__main__':
