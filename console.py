cf810e6eff41b0ed9a9b7e706b0b54e66fd0614c
Adeosun Oluwaseyi <deosundeola@gmail.com>
2023-08-12 17:55:18 -0700

    changes

--- console.py
+++ console.py
@@ -4,6 +4,7 @@ Class console command that opens cmd interface to edit and update files
 """
 import cmd
 from models import storage
+from models.engine.file_storage import FileStorage
 from models.base_model import BaseModel
 from models.user import User
 from models.state import State
@@ -73,19 +74,64 @@ class HBNBCommand(cmd.Cmd):
         new.save()
         print(new.id)
 
+    def help_create(self):
+        """
+        Display the help for the create command.
+        """
+        print("Create a new instance of a specified"
+              "class and save it to a JSON file")
+
     def do_show(self, args):
         """
         Display the string representation of an instance.
         Usage: show User <id>
         """
-        self._validate_show_and_destroy(arg, "destroy")
+        self._validate_show_and_destroy(args, "destroy")
 
-    def do_destroy(Self, args):
+    def help_show(self):
+        """
+        Display help for the show command.
+        """
+        print("Display the string representation of an instance")
+
+    def do_destroy(self, args):
         """
         Deletes an instance based on the class name and id.
         Usage: destroy User <id>
         """
         self._validate_show_and_destroy(args, "destroy")
+
+    def help_destroy(self):
+        """
+        Display help for the destroy command.
+        """
+        print("Delete an instance based on the class name and id")
+
+    def do_all(self, args):
+        """
+        Display string representation of all instance or all instance
+        of a specific class.
+        Usage: all[class_name]
+        """
+        storage = FileStorage()
+        obj_dict = storage.all()
+        if not args:
+            for obj_key, obj in obj_dict.items():
+                print(obj)
+        elif args in self.classes:
+            for obj_key, obj in obj_dict.items():
+                if obj_key.split('.')[0] == args:
+                    print(obj)
+        else:
+            print("** class doesn't exist **")
+
+    def help_all(self):
+        """
+        Display help for all command
+        """
+        print("Display string representations of all instances,"
+              "or all instances of a specific class")
+        print("Usage: all[class_name]")
 
     def do_update(self, args):
         """
@@ -94,4 +140,63 @@ class HBNBCommand(cmd.Cmd):
         """
         self._validate_update(args)
 
-    def _validate_show_and_destroy(self, arg, 
+    def help_update(self):
+        """
+        Display help for the update command.
+        """
+        print("Update an instance based on the class name and id")
+
+    def _validate_show_and_destroy(self, args, act):
+        if not args:
+            print(f"** class name missing **")
+            return
+        arg = args.split()
+        if arg[0] not in self.classes:
+            print("** class doesn't exist **")
+            return
+        if len(arg) < 2:
+            print(f"** instance id missing **")
+            return
+        storage = FileStorage()
+        obj_dict = storage.all()
+        obj_key = f"{arg[0]}.{arg[1]}"
+        if obj_key not in obj_dict:
+            print("** no instance found **")
+            return
+        print(obj_dict[obj_key])
+        if act == "show":
+            print(obj_dict[obj_key])
+        elif act == "destroy":
+            del obj_dict[obj_key]
+            storage.save()
+
+    def _validate_update(self, args):
+        if not args:
+            print("** class name missing **")
+            return
+        arg = args.split()
+        if arg[0] not in self.classes:
+            print("** class doesn't  exist **")
+            return
+        if len(arg) < 2:
+            print("** instance id missing **")
+            return
+        storage = FileStorage()
+        obj_dict = storage.all()
+        obj_key = f"{arg[0]}.{arg[1]}"
+        if obj_key not in obj_dict:
+            print("** no instance found **")
+            return
+        if len(arg) < 3:
+            print("** attribute name missing **")
+            return
+        if len(arg) < 4:
+            print("** value missing **")
+            return
+        instance = obj_dict[obj_key]
+        setattr(instance, args[2], args[3])
+        instance.save()
+
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
